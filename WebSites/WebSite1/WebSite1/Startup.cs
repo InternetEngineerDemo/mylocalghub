2da6d1febbdbe8485616b67a3423ecf02f8c538c
InternetEngineer <InternetEngineer@Demo>
2016-06-17 00:33:56 -0400

    My First Commit

---
+++ WebSites/WebSite1/WebSite1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite1.Startup))]
+namespace WebSite1
+{
+    //Hello World
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
